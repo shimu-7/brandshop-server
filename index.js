e8b960a6cc178d4729fc83b43c6741b2bfce3698
shimu-7 <shamima19shimu@gmail.com>
2023-10-24 05:40:11 +0600

    delete api is created to remove product from cart

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -54,6 +54,14 @@ async function run() {
         app.get('/cart', async (req, res) => {
             const cursor = cart.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.delete('/cart/:id', async (req,res)=>{
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)}
+
+            const result = await cart.deleteOne(query);
             res.send(result);
         })
         // Send a ping to confirm a successful connection
